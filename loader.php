d397a08b5d44a2b0d30ec2215efe510f44d57c1e
Matvey Vishnevsky <mc-matuhak@ya.ru>
2019-11-26 18:22:19 +0300

    Not beta: 0.5-beta changed to 0.5

--- loader.php
+++ loader.php
@@ -2,7 +2,7 @@
 
 if(!extension_loaded('curl')) exit('I need curl extension to run');
 
-define("SEV", "0.5-beta");
+define("SEV", "0.5");
 define("NEED_LP_LOGS", false);
 
 include __DIR__.'/config.php';
