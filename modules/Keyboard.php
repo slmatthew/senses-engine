f537b095894211d6ea49e0750e6bb353a2e848c9
Matvey Vishnevsky <mc-matuhak@ya.ru>
2019-11-29 19:42:11 +0300

    v0.6: void -> Keyboard

--- modules/Keyboard.php
+++ modules/Keyboard.php
@@ -39,39 +39,45 @@ class Keyboard {
 	 * Constructor
 	 * @param bool $one_time Should VK hide the keyboard after first use
 	 * @param bool $inline Should keyboard be inline
-	 * @return void
+	 * @return Keyboard
 	 * @since v0.3
 	 */
 	public function __construct(bool $one_time = false, bool $inline = false) {
 		$this->one_time = $one_time;
 		$this->inline = $inline;
+
+		return $this;
 	}
 
 	/**
 	 * Should VK hide the keyboard after first use
 	 * @param bool $enabled True or false
-	 * @return void
+	 * @return Keyboard
 	 * @since 0.6
 	 */
 	public function oneTime(bool $enabled) {
 		$this->one_time = $enabled;
+
+		return $this;
 	}
 
 	/**
 	 * Should keyboard be inline
 	 * @param bool $enabled True or false
-	 * @return void
+	 * @return Keyboard
 	 * @since 0.6
 	 */
 	public function inline(bool $enabled) {
 		$this->one_time = $enabled;
+
+		return $this;
 	}
 
 	/**
 	 * Button constructor
 	 * @param array $action https://vk.com/dev/bots_docs_3
 	 * @param string $color Button color
-	 * @return void
+	 * @return Keyboard
 	 * @since v0.3
 	 */
 	public function addButton(array $action, string $color = '') {
@@ -87,6 +93,8 @@ class Keyboard {
 				'action' => $action
 			];
 		}
+
+		return $this;
 	}
 
 	/**
@@ -94,7 +102,7 @@ class Keyboard {
 	 * @param string $label Text on the button
 	 * @param array $payload Payload
 	 * @param string $color Button color
-	 * @return void
+	 * @return Keyboard
 	 * @since 0.6
 	 */
 	public function addTextButton(string $label, array $payload = [], string $color = self::PRIMARY_BUTTON) {
@@ -103,12 +111,14 @@ class Keyboard {
 			'label' => $label,
 			'payload' => $this->toJson($payload)
 		], $color);
+
+		return $this;
 	}
 
 	/**
 	 * Location button constructor
 	 * @param array $payload Payload
-	 * @return void
+	 * @return Keyboard
 	 * @since 0.6
 	 */
 	public function addLocationButton(array $payload = []) {
@@ -116,13 +126,15 @@ class Keyboard {
 			'type' => 'location',
 			'payload' => $this->toJson($payload)
 		]);
+
+		return $this;
 	}
 
 	/**
 	 * VK Pay button constructor
 	 * @param array $hash Hash
 	 * @param array $payload Payload
-	 * @return void
+	 * @return Keyboard
 	 * @since 0.6
 	 */
 	public function addPayButton(array $hash, array $payload = []) {
@@ -131,6 +143,8 @@ class Keyboard {
 			'hash' => http_build_query($hash),
 			'payload' => $this->toJson($payload)
 		]);
+
+		return $this;
 	}
 
 	/**
@@ -140,7 +154,7 @@ class Keyboard {
 	 * @param string $label Text on the button
 	 * @param string $hash Hash: https://vk.com/app123#{hash}
 	 * @param array $payload
-	 * @return void
+	 * @return Keyboard
 	 * @since 0.6
 	 */
 	public function addAppButton(int $app_id, int $owner_id, string $label, string $hash = '', array $payload = []) {
@@ -152,15 +166,19 @@ class Keyboard {
 			'hash' => $hash,
 			'payload' => $this->toJson($payload)
 		]);
+
+		return $this;
 	}
 
 	/**
 	 * Line constructor
-	 * @return void
+	 * @return Keyboard
 	 * @since v0.3
 	 */
 	public function addLine() {
 		$this->currentIndex = $this->currentIndex + 1;
+
+		return $this;
 	}
 
 	/**
