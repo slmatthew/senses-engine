1e5272a19b635383f27881cb8d7e3dba39103b6b
Matvey Vishnevsky <mc-matuhak@ya.ru>
2020-02-16 18:51:16 +0300

    loader: updated to 0.8, incude > include_once, namespaces

--- loader.php
+++ loader.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Senses;
+
 /**
  * @package settings
  */
@@ -9,26 +11,27 @@ if(!extension_loaded('curl')) exit('I need curl extension to run');
 /**
  * @ignore
  */
-define("SEV", "0.7");
+define("SEV", "0.8");
 
 /**
  * @var bool Should engine show LP logs
  */
 define("NEED_LP_LOGS", false);
 
-include __DIR__.'/config.php';
+include_once __DIR__.'/config.php';
 
 if(isset($config['dev'])) define("CURL_VERIFY", false);
 else define("CURL_VERIFY", true);
 
-include __DIR__.'/modules/Exceptions.php';
-include __DIR__.'/modules/Requests.php';
-include __DIR__.'/modules/BotEngine.php';
-include __DIR__.'/modules/SBSC.php';
-include __DIR__.'/modules/DataHandler.php';
-include __DIR__.'/modules/Keyboard.php';
-include __DIR__.'/modules/Template.php';
-include __DIR__.'/modules/LpDecoder.php';
-include __DIR__.'/modules/Audio.php';
+include_once __DIR__.'/modules/Exceptions.php';
+include_once __DIR__.'/modules/Requests.php';
+include_once __DIR__.'/modules/BotEngine.php';
+include_once __DIR__.'/modules/SBSC.php';
+include_once __DIR__.'/modules/DataHandler.php';
+include_once __DIR__.'/modules/Keyboard.php';
+include_once __DIR__.'/modules/Template.php';
+include_once __DIR__.'/modules/LpDecoder.php';
+include_once __DIR__.'/modules/Audio.php';
+include_once __DIR__.'/modules/Vk.php';
 
 ?>
