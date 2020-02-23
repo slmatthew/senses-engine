0a489ddecd618f6451a621bb138ebd8924e4d1d4
Matvey Vishnevsky <mc-matuhak@ya.ru>
2020-02-24 01:46:33 +0300

    requests: changed vk api version to 5.118

--- modules/Requests.php
+++ modules/Requests.php
@@ -41,7 +41,7 @@ function call(string $method, array $params, bool $official = false) {
 	global $config;
 
 	if(!isset($params['access_token'])) $params['access_token'] = isset($config['token']) ? $config['token'] : '';
-	if(!isset($params['v'])) $params['v'] = isset($config['version']) ? $config['version'] : '5.103';
+	if(!isset($params['v'])) $params['v'] = isset($config['version']) ? $config['version'] : '5.118';
 	if(isset($params['unsetToken']) && $params['unsetToken']) unset($params['access_token']);
 
 	$agent = $official ? "VKAndroidApp/5.50-4431 (1; 1; 1; 1; 1; 1)" : "Senses Bot Engine/".SEV;
