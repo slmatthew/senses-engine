4318258e8d29290fd12d46e2a871f82dc8ce8ba3
Matvey Vishnevsky <mc-matuhak@ya.ru>
2020-03-10 23:20:19 +0300

    chore(loader): fixes

--- loader.php
+++ loader.php
@@ -9,28 +9,31 @@ if(!extension_loaded('curl')) exit('I need curl extension to run');
 /**
  * @ignore
  */
-define("SEV", "0.8");
+define("SEV", "0.9");
 
 /**
  * @var bool Should engine show LP logs
  */
 define("NEED_LP_LOGS", false);
 
-if(isset($config['dev'])) define("CURL_VERIFY", false);
-else define("CURL_VERIFY", true);
+/**
+ * @var bool
+ */
+define("CURL_VERIFY", true);
 
 include_once __DIR__.'/modules/Exceptions.php';
-include_once __DIR__.'/modules/Message.php';
 include_once __DIR__.'/modules/Requests.php';
+include_once __DIR__.'/modules/Message.php';
+include_once __DIR__.'/modules/LpDecoder.php';
 include_once __DIR__.'/modules/BotEngine.php';
-//include_once __DIR__.'/modules/SBSC.php';
 include_once __DIR__.'/modules/DataHandler.php';
 include_once __DIR__.'/modules/Keyboard.php';
 include_once __DIR__.'/modules/Template.php';
-include_once __DIR__.'/modules/LpDecoder.php';
 include_once __DIR__.'/modules/Audio.php';
 include_once __DIR__.'/modules/Vk.php';
+include_once __DIR__.'/modules/Execute.php';
 include_once __DIR__.'/modules/Auth.php';
+include_once __DIR__.'/modules/Upload.php';
 include_once __DIR__.'/modules/Debugger.php';
 
 ?>
