56c49f7c280698116fda0d989937111b7a65d419
Matvey Vishnevsky <mc-matuhak@ya.ru>
2020-05-30 14:55:13 +0300

    modules(exceptions): add 2 new exceptions

--- src/Exceptions.php
+++ src/Exceptions.php
@@ -3,5 +3,19 @@
 namespace slmatthew\senses;
 
 class InvalidAuthDataException extends \Exception {}
+class LostConnectionException extends \Exception {}
+
+class UnableToAuthException extends \Exception {
+	private array $result = [];
+
+	public function __construct(array $result) {
+		Exception::__construct('Unable to auth');
+		$this->result = $result;
+	}
+
+	public function getResult() {
+		return $this->result;
+	}
+}
 
 ?>
